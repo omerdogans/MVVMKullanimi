fd37a9cd500880c5133ab1faf580f84e892bf862
Omer Dogan <omerdogan.27gs@gmail.com>
2023-04-06 14:02:25 +0300

    Version 1.0.0

---
+++ app/src/main/java/com/example/mvvmkullanimi/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mvvmkullanimi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.mvvmkullanimi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivityNesnesi = this
+
+        viewModel.sonuc.observe(this) {
+            binding.hesaplamaSonucu = it
+        }
+
+    }
+
+    fun buttonToplamaTikla(alinanSayi1: String, alinanSayi2: String) {
+        viewModel.toplamaYap(alinanSayi1, alinanSayi2)
+
+    }
+
+    fun buttonCarpmaTikla(alinanSayi1: String, alinanSayi2: String) {
+        viewModel.carpmaYap(alinanSayi1, alinanSayi2)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/mvvmkullanimi/MatematikRepository.kt
@@ -0,0 +1,36 @@
+package com.example.mvvmkullanimi
+
+import androidx.lifecycle.MutableLiveData
+
+class MatematikRepository {
+
+    var matematikselSonuc = MutableLiveData<String>()
+
+    init {
+        matematikselSonuc = MutableLiveData("0")
+    }
+
+    fun matematikselSonucuGetir() : MutableLiveData<String>{
+
+        return matematikselSonuc
+
+    }
+
+    fun topla(alinanSayi1:String, alinanSayi2:String){
+        val sayi1 = alinanSayi1.toInt()
+        val sayi2 = alinanSayi2.toInt()
+
+        val toplam = sayi1 + sayi2
+
+        matematikselSonuc.value = toplam.toString()
+    }
+
+    fun carp(alinanSayi1:String, alinanSayi2:String){
+        val sayi1 = alinanSayi1.toInt()
+        val sayi2 = alinanSayi2.toInt()
+
+        val carpma = sayi1 * sayi2
+
+        matematikselSonuc.value = carpma.toString()
+    }
+}
